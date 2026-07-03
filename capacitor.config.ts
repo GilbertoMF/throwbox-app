f17c48a727c095f498dcba9abd3466b183935e81
GilbertoMF <gilbertomartins900@gmail.com>
2026-07-03 10:21:11 -0300

    Change server scheme to http on Android to align with Authorized origins

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.throwbox.app',
   appName: 'Throwbox',
   webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    hostname: 'localhost'
+  },
   android: {
     overrideUserAgent: 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'
   }
